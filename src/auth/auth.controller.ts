1a77344f5cccb6270e09a9816732d473a40147bd
karthiksuryana <karthik.surya@c1exchange.com>
2025-08-21 15:36:21 +0530

    Authentication added for Signup and login

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto, RegisterDto } from 'src/models/user.dto';
+
+@Controller('users')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  register(@Body(ValidationPipe) credentials: RegisterDto) {
+    return this.authService.register(credentials);
+  }
+
+  @Post('login')
+  @UsePipes(ValidationPipe)
+  login(@Body(ValidationPipe) credentials: LoginDto) {
+    return this.authService.login(credentials);
+  }
+}
