dc576683f72462dfaec09126f5cab52c8356901f
karthiksuryana <karthik.surya@c1exchange.com>
2025-08-21 13:30:03 +0530

    Database Connection using Typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogModule } from './blog/blog.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConnectionService } from './database.connection';
 
 @Module({
-  imports: [],
+  imports: [
+    BlogModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: DatabaseConnectionService }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
